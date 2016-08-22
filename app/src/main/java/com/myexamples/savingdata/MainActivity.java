01b58a16a939e980d426dcb33f43bd2859f479d5
gavransava <gavransava@gmail.com>
2016-08-22 23:29:40 +0200

    Insert new text to database and list the contents in separate text view.

--- app/src/main/java/com/myexamples/savingdata/MainActivity.java
+++ app/src/main/java/com/myexamples/savingdata/MainActivity.java
@@ -1,10 +1,15 @@
 package com.myexamples.savingdata;
 
+import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.myexamples.savingdata.database.TextContract;
 import com.myexamples.savingdata.database.TextDBHelper;
@@ -22,6 +27,21 @@ public class MainActivity extends AppCompatActivity {
 
         mDBHelper = new TextDBHelper(this);
         mDatabase = mDBHelper.getWritableDatabase();
+        TextView textView = (TextView) findViewById(R.id.textView3);
+        textView.setMovementMethod(new ScrollingMovementMethod());
+        new QueryTextTask(QUERY_ALL_VALUE).execute();
+    }
+
+    public void onClick(View view){
+        EditText editText = (EditText)findViewById(R.id.editText);
+        String text = editText.getText().toString();
+        insertText(text);
+    }
+
+    public void insertText(String text) {
+        ContentValues cv = new ContentValues();
+        cv.put(TextContract.Text.TEXT, text);
+        mDatabase.insert(TextContract.Text.TABLE_NAME, null, cv);
         new QueryTextTask(QUERY_ALL_VALUE).execute();
     }
 
@@ -41,6 +61,20 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(Cursor c) {
+            if(mId == QUERY_ALL_VALUE) {
+                if (c.getCount() < 1)
+                    return;
+
+                c.moveToLast();
+                StringBuilder textToDisplay = new StringBuilder(c.getString(c.getColumnIndex(TextContract.Text.TEXT)));
+                textToDisplay.append("\n\n");
+                while(c.moveToPrevious()){
+                    textToDisplay.append(c.getString(c.getColumnIndex(TextContract.Text.TEXT))).append("\n\n");
+                }
+                c.close();
+                TextView textView = (TextView) findViewById(R.id.textView3);
+                textView.setText(textToDisplay);
+            }
         }
     }
 }
